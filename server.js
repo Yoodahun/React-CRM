4d04e634c0f209b31d1d06105f1d857b6d21bc08
dahun <dahun4032@gmail.com>
2019-07-31 20:52:28 +0900

    [UPDATE] CRM

--- server.js
+++ server.js
@@ -1,36 +1,43 @@
 const express = require('express')
-const cowsay = require('cowsay')
-const cors = require('cors')
-// Create the server
-const app = express()
+const bodyParser = require('body-parser');
+const fs = require('fs'); //file read
+const data = fs.readFileSync('./database.json'); //read .database.json
+const conf = JSON.parse(data);
 
 const path = require('path')
+// Create the server
+const app = express()
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'client/build')))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
 // Anything that doesn't match the above, send back index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
-// Serve our api route /cow that returns a custom talking text cow
-app.get('/api/cow/:say', cors(), async (req, res, next) => {
-  try {
-    const text = req.params.say
-    const moo = cowsay.say({ text })
-    res.json({ moo })
-  } catch (err) {
-    next(err)
-  }
-})
-// Serve our base route that returns a Hello World cow
-app.get('/api/cow/', cors(), async (req, res, next) => {
-  try {
-    const moo = cowsay.say({ text: 'Hello World!' })
-    res.json({ moo })
-  } catch (err) {
-    next(err)
-  }
-})
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: conf.HOST,
+  user: conf.USER,
+  password: conf.PASSWORD,
+  port: conf.PORT,
+  database: conf.DATABASE_NAME
+});
+connection.connect();
+
+
+
+app.get('/api/customers', (req, res) => {
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
+});
 
 // Choose the port and start the server
 const PORT = process.env.PORT || 5000

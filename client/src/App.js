a9d1738759a32bcc861c94850add6f3e23a53a77
dahun <dahun4032@gmail.com>
2019-07-31 20:42:35 +0900

    Checkout branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,98 @@
-import React, { Component } from 'react'
-import './App.css'
-class App extends Component {
-  state = {
-    cow: '',
-    text: ''
+import React from 'react';
+// import './App.css';
+import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const styles = (theme) => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing(2)
   }
-componentDidMount() {
-    this.fetchCow()
+})
+
+//main file. 실질적으로 웹사이트에 내용 출력을 담당함.
+
+// console.log(this.props);
+class App extends React.Component {
+  // function App() {
+  // const { classes } = this.props;
+
+  state = { //데이터가 변경될 수 있을 때는 컴포넌트 내에서 변경될 수 있는 변수를 state로 선언함.
+    customers: "",
+    completed: 0
   }
-fetchCow = async () => {
-    const response = await fetch(`/api/cow`)
-    const initialCow = await response.json()
-    const cow = initialCow.moo
-    this.setState({ cow })
+
+  componentDidMount() { //api서버에 변경된 데이터를 받아오는 작업은 componentDidMount. 모든 컴포넌트가 준비가 되었을 때.
+    this.timer = setInterval( this.progress, 20); //timer를 이용하여 20초마다 한 번씩 실행해줌.
+    this.callApi()
+      .then(res => this.setState({customers: res}))//호출한 callApi의 Response를 customer에 셋팅해주는 것.
+      .catch(err => console.log(err)); //에러가 오면 에러 캐치
+
   }
-customCow = async evt => {
-    evt.preventDefault()
-    const text = this.state.text
-    const response = await fetch(`/api/cow/${text}`)
-    const custom = await response.json()
-    const cow = custom.moo
-    this.setState({ cow, text: '' })
+
+  callApi = async () => { //비동기 처리작업.
+    const response = await fetch('/api/customers'); //정의된 주소에 접근하여
+    const body = await response.json(); //response를 json으로 받
+    return body;
   }
-handleChange = evt => {
-    this.setState({ [evt.target.name]: evt.target.value })
-    console.log(this.state.text)
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({
+      completed: completed >= 100 ? 0 : completed + 1
+    });
   }
-render() {
+
+  render() {
     return (
-      <div className="App">
-        <h3>Text Cow. Moo</h3>
-        <code>{this.state.cow}</code>
-        <form onSubmit={this.customCow}>
-          <label>Custom Cow Text:</label>
-          <input
-            type="text"
-            name="text"
-            value={this.state.text}
-            onChange={this.handleChange}
-          />
-          <button type="submit">Show me a talking cow!</button>
-        </form>
-      </div>
-    )
+      <div>
+        <Paper className={this.props.classes.root}>
+          <Table className={this.props.classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>No.</TableCell>
+                <TableCell>Image</TableCell>
+                <TableCell>名前</TableCell>
+                <TableCell>生年月日</TableCell>
+                <TableCell>性別</TableCell>
+                <TableCell>職業</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {
+                this.state.customers ? this.state.customers.map(c => {
+                  return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>)
+                }) :
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress className={this.props.classes.progress} variant="determinate" value={this.state.completed}/>
+
+                  </TableCell>
+                </TableRow>
+              }
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd/>
+    </div>
+  );
   }
 }
-export default App
+
+// export default App;
+export default withStyles(styles)(App);
---
+++ client/src/components/CustomerAdd.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { post } from 'axios';
+
+class CustomerAdd extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null,
+      userName: '',
+      birthday: '',
+      gender: '',
+      job: '',
+      fileName: ''
+    }
+  }
+
+handleFormSubmit = (e) => {//submit버튼을 눌렀을 때, 해당 input으로 부터 데이터가 들어오는 것.
+  e.preventDefault() //파일이 제대로 올 수 있도록?
+  this.addCustomer().then((response) => { //서버로부터 어떠한 값을 되받았을 때
+    console.log(response.data);
+
+  })
+}
+
+handleFileChange = (e) => { //submit버튼을 눌렀을 때, 해당 input으로 부터 데이터가 들어오는 것.
+  this.setState({
+    file: e.target.files[0], //target은 event가 발생한 지점 그 자체. 왜하필 첫번째냐면 하나의 파일만 업데이트할 것.
+    fileName: e.target.value
+  })
+}
+
+handleValueChange = (e) => {//submit버튼을 눌렀을 때, 해당 input으로 부터 데이터가 들어오는 것.
+  let nextState = {};
+  nextState[e.target.name] = e.target.value;
+  this.setState(nextState);
+}
+
+addCustomer = () => {
+  const url = 'api/customers';
+  const formData = new FormData();
+  formData.append('image', this.state.file);
+  formData.append('name', this.state.userName);
+  formData.append('birthday', this.state.birthday);
+  formData.append('gender', this.state.gender);
+  formData.append('job', this.state.job);
+
+  const config = { //환경설정
+    headers: {
+      'content-type': 'multypart/form-data'
+    }
+  }
+
+  return post(url, formData, config); //post방식으로 해당 url에 어떠한 파일데이터를 가지고 설정을하여 접근함.
+
+}
+
+render() { //실제 이미지가 렌더링되어 표시되는 부분
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <h1> 고객 추가 </h1>
+        프로필 이미지 : <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}></input>
+        <br/>
+        이름 : <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}></input>
+        <br/>
+        생년월일 : <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}></input>
+        <br/>
+        성별 : <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}></input>
+        <br/>
+         직업 : <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}></input>
+       <button type="submit">추가하기</button>
+      </form>
+    )
+}
+
+
+}
+
+export default CustomerAdd;

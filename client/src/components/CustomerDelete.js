500e34cdfe8d1e9e308a121fa0f03822008fbba6
dahun <dahun4032@gmail.com>
2019-08-06 23:15:32 +0900

    [NEW] Material UI Design for add and delete

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from  '@material-ui/core/DialogTitle';
+import DialogContent from  '@material-ui/core/DialogContent';
+import Button from  '@material-ui/core/Button';
+import Typography from  '@material-ui/core/Typography';
+
 
 class CustomerDelete extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
+
+    handleClickOpen = () => { // 고객추가하기 버튼을 눌렀을 때  바인딩처리
+        this.setState({
+            open: true
+        });
+    }
+
+    handleClose  = () => { // 고객추가하기 모달을 닫았을 때 바인딩처리
+        this.setState({
+            open: false
+        });
+
+    }
 
     deleteCustomer(id) {
         const url = '/api/customers/' + id;
@@ -12,7 +39,20 @@ class CustomerDelete extends React.Component{
 
     render() {
         return  (
-          <button onClick={(event => {this.deleteCustomer(this.props.id)})}> Delete </button>
+            <div>
+              <Button variant="contained" color="secondary"  onClick={this.handleClickOpen}> Delete </Button>
+              <Dialog open={this.state.open} onClose={this.handleClose}>
+                <DialogTitle> Delete Message </DialogTitle>
+                <DialogContent>
+                    <Typography gutterBottom> 選択した情報は削除されますよ。よろしいでしょうか？　</Typography>
+                    {/*gutterBottom = 밑에 마진을 넣음*/}
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="contained" color="primary" onClick={(event) => {this.deleteCustomer(this.props.id)}}> 削除 </Button>
+                    <Button variant="outlined" color="primary" onClick={(event) => {this.handleClose()}}> キャンセル </Button>
+                </DialogActions>
+              </Dialog>
+            </div>
         );
     }
 }
